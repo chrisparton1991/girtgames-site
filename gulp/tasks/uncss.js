2e3f183740f9ac8869f62c2f66e0f3ac859a974d
Chris Parton <chris.parton@finocomp.com>
2017-07-19 13:18:07 +1000

    Removed unneeded uncss ignores.

--- gulp/tasks/uncss.js
+++ gulp/tasks/uncss.js
@@ -3,18 +3,13 @@ var gulp = require('gulp');
 var uncss = require('gulp-uncss');
 
 gulp.task('uncss', ['html', 'sass'], function () {
-    var appIgnores = [/\.finocomp-power-button/];
-    var owlCarouselIgnores = [/\.owl/];
-    var bootStrapIgnores = [
-        /\.affix/, /\.alert/, /\.close/, /\.collaps/, /\.fade/, /\.has/, /\.help/, /\.active/,
-        /\.in/, /\.modal/, /\.open/, /\.popover/, /\.tooltip/, /\.top-nav-collapse/];
+    var bootstrapIgnores = [/\.active/];
 
-    var ignores = appIgnores.concat(owlCarouselIgnores).concat(bootStrapIgnores);
     return gulp
         .src(config.uncss.cssSrc)
         .pipe(uncss({
             html: [config.uncss.htmlSrc],
-            ignore: ignores
+            ignore: bootstrapIgnores
         }))
         .pipe(gulp.dest(config.uncss.cssDest));
 });
